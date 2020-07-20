4946d30d71d7b800ee64ba6aa578dbe3e4a028a1
Luis H. P. Mendes <luishpmendes@gmail.com>
2020-07-20 10:49:25 -0300

    Argument Parser class
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

---
+++ src/utils/argument_parser.cpp
@@ -0,0 +1,24 @@
+#include "utils/argument_parser.hpp"
+#include <algorithm>
+
+ArgumentParser::ArgumentParser(int argc, char * argv[]) {
+    for (int i = 1; i < argc; i++) {
+        this->tokens.push_back(std::string(argv[i]));
+    }
+}
+
+std::string ArgumentParser::option_value(const std::string & option) const {
+    auto it = std::find(this->tokens.begin(), this->tokens.end(), option);
+
+    if (it != this->tokens.end() && ++it != this->tokens.end()) {
+        return *it;
+    }
+
+    return std::string("");
+}
+
+bool ArgumentParser::option_exists(const std::string & option) const {
+    return std::find(this->tokens.begin(), this->tokens.end(), option) !=
+        this->tokens.end();
+}
+
---
+++ src/utils/argument_parser.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class ArgumentParser {
+public:
+    /********************************
+     * The tokens.
+     ********************************/
+    std::vector<std::string> tokens;
+
+    /****************************************
+     * Constructs a new argument parser.
+     *
+     * @param argc the number of arguments.
+     * @param argv the array of arguments.
+     ****************************************/
+    ArgumentParser(int argc, char * argv[]);
+
+    /***********************************************************
+     * Returns the value of the specified option.
+     *
+     * @param the option whose value is to be returned.
+     *
+     * @return the value of the specified option.
+     ***********************************************************/
+    std::string option_value(const std::string & option) const;
+
+    /****************************************************************
+     * Verifies whether the specified option exists.
+     *
+     * @param option the option whose existence is to be verified.
+     *
+     * @return true if the specified option exists; false otherwise.
+     ****************************************************************/
+    bool option_exists(const std::string & option) const;
+};
+
