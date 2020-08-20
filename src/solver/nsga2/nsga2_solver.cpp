b6c35f86f8b6509cc597883b640be222aca4f937
Luis H. P. Mendes <luishpmendes@gmail.com>
2020-08-20 18:51:53 -0300

    NSGA2 Solver class
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

---
+++ src/solver/nsga2/nsga2_solver.cpp
@@ -0,0 +1,135 @@
+#include "solver/nsga2/nsga2_solver.hpp"
+#include "solver/nsga2/problem.hpp"
+#include <pagmo/algorithms/nsga2.hpp>
+
+namespace motsp {
+
+NSGA2_Solver::NSGA2_Solver(const Instance & instance)
+    : Solver::Solver(instance) {}
+
+bool NSGA2_Solver::update_best_individuals(const pagmo::population & pop) {
+    std::vector<std::pair<std::vector<double>, std::vector<double>>>
+        new_individuals(pop.size());
+
+    for(unsigned i = 0; i < pop.size(); i++) {
+        new_individuals[i] = std::make_pair(pop.get_f()[i], pop.get_x()[i]);
+    }
+
+    return Solver::update_best_individuals(new_individuals);
+}
+
+void NSGA2_Solver::capture_snapshot(const pagmo::population & pop) {
+    this->pareto.resize(this->best_individuals.size());
+    std::transform(this->best_individuals.begin(),
+                   this->best_individuals.end(),
+                   this->pareto.begin(),
+                   [](const auto & individual) {
+                        return individual.first;
+                   });
+    this->pareto_snapshots.push_back(std::make_tuple(this->num_iterations,
+                                                     this->elapsed_time(),
+                                                     this->pareto));
+
+    this->current_individuals.resize(pop.size());
+
+    for(unsigned i = 0; i < pop.size(); i++) {
+        this->current_individuals[i] = std::make_pair(pop.get_f()[i],
+                                                      pop.get_x()[i]);
+    }
+
+    this->fronts = BRKGA::Population::nonDominatedSort<std::vector<double>>(
+            current_individuals,
+            this->senses);
+
+    this->non_dominated_snapshots.push_back(std::make_tuple(
+                this->num_iterations,
+                this->elapsed_time(),
+                std::vector<unsigned>(1, fronts.front().size())));
+
+    this->fronts_snapshots.push_back(std::make_tuple(
+                this->num_iterations,
+                this->elapsed_time(),
+                std::vector<unsigned>(1, fronts.size())));
+
+    this->time_last_snapshot = this->elapsed_time();
+    this->iteration_last_snapshot = this->num_iterations;
+    this->num_snapshots++;
+}
+
+void NSGA2_Solver::solve() {
+    this->start_time = std::chrono::steady_clock::now();
+
+    pagmo::problem prob{Problem(this->instance)};
+
+    pagmo::algorithm algo{pagmo::nsga2(1,
+                                       this->crossover_probability,
+                                       this->crossover_distribution,
+                                       this->mutation_probability,
+                                       this->mutation_distribution,
+                                       this->seed)};
+
+    if(this->max_num_snapshots > 0) {
+        this->time_between_snapshots = this->time_limit /
+            this->max_num_snapshots;
+        this->iterations_between_snapshots = this->iterations_limit /
+            this->max_num_snapshots;
+    }
+
+    pagmo::population pop{
+        prob,
+        this->population_size - this->initial_individuals.size(),
+        this->seed};
+
+    for(const auto & individual : this->initial_individuals) {
+        pop.push_back(individual.second, individual.first);
+    }
+
+    this->update_best_individuals(pop);
+
+    if(this->max_num_snapshots > 0) {
+        this->capture_snapshot(pop);
+    }
+
+    while(!this->are_termination_criteria_met()) {
+        this->num_iterations++;
+        pop = algo.evolve(pop);
+        this->update_best_individuals(pop);
+
+        if(this->max_num_snapshots > 0 &&
+           this->num_snapshots < this->max_num_snapshots &&
+          (this->elapsed_time() - this->time_last_snapshot >=
+           this->time_between_snapshots ||
+           this->num_iterations - this->iteration_last_snapshot >=
+           this->iterations_between_snapshots)) {
+            this->capture_snapshot(pop);
+        }
+    }
+
+    if(this->max_num_snapshots > 0) {
+        this->capture_snapshot(pop);
+    }
+
+    this->best_solutions.clear();
+
+    for(const auto & best_individual : this->best_individuals) {
+        this->best_solutions.push_back(Solution(this->instance,
+                                                best_individual.second));
+    }
+
+    this->solving_time = this->elapsed_time();
+}
+
+std::ostream & operator <<(std::ostream & os, const NSGA2_Solver & solver) {
+    os << static_cast<const Solver &>(solver)
+       << "Population size: " << solver.population_size << std::endl
+       << "Crossover probability: " << solver.crossover_probability << std::endl
+       << "Distribution index for crossover: " << solver.crossover_distribution
+       << std::endl
+       << "Mutation probability: " << solver.mutation_probability << std::endl
+       << "Distribution index for mutation: " << solver.mutation_distribution
+       << std::endl;
+    return os;
+}
+
+}
+
---
+++ src/solver/nsga2/nsga2_solver.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "solver/solver.hpp"
+#include <pagmo/population.hpp>
+
+namespace motsp {
+/****************************************************
+ * The NSGA2_Solver represents a solver for the
+ * Multi-Objective Travelling Salesman Problem using
+ * the Non-Dominated Sorting Genetic Algorithm II.
+ ****************************************************/
+class NSGA2_Solver : public Solver {
+    public:
+    /********************************
+     * Size of the population.
+     ********************************/
+    unsigned population_size = 1000;
+
+    /************************************
+     * Crossover probability.
+     ************************************/
+    double crossover_probability = 0.95;
+
+    /**************************************
+     * Distribution index for crossover.
+     **************************************/
+    double crossover_distribution = 10.00;
+
+    /***********************************
+     * Mutation probability.
+     ***********************************/
+    double mutation_probability = 0.01;
+
+    /*************************************
+     * Distribution index for mutation.
+     *************************************/
+    double mutation_distribution = 50.00;
+
+    /*********************************************
+     * Constructs a new solver.
+     *
+     * @param instance the instance to be solved.
+     *********************************************/
+    NSGA2_Solver(const Instance & instance);
+
+    /**********************************************************************
+     * Update the best individuals found so far.
+     *
+     * @param pop the new solutions.
+     *
+     * @return true if the best individuals are modified; false otherwise.
+     **********************************************************************/
+    bool update_best_individuals(const pagmo::population & pop);
+
+    /*****************************************************
+     * Capture a snapshot of the current population.
+     *
+     * @param pop the current population.
+     *****************************************************/
+    void capture_snapshot(const pagmo::population & pop);
+
+    /**********************
+     * Solve the instance.
+     **********************/
+    void solve();
+
+    /***************************************************************
+     * Standard stream operator.
+     *
+     * @param os the standard output stream object.
+     * @param solver the solver.
+     *
+     * @return the stream object.
+     ***************************************************************/
+    friend std::ostream & operator <<(std::ostream & os,
+                                      const NSGA2_Solver & solver);
+};
+
+}
+
